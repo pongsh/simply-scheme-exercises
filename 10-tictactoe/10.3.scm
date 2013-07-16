848c69ddf6ae3fe65f0d2b1a1ec2b08b809b2c40
Patrick Ong <p.ongsh@gmail.com>
2013-07-16 22:32:59 +0800

    Fixed 10.3 to work with edge cases

--- 10-tictactoe/10.3.scm
+++ 10-tictactoe/10.3.scm
@@ -1,7 +1,17 @@
 (define (tie-game? position)
-  (let ((triples (find-triples position)))
     (and (<= (appearances '_ position) 2)
-         (not (already-won? position 'x))
-         (not (already-won? position 'o))
-         (not (i-can-win? triples 'x))
-         (not (i-can-win? triples 'o)))))
+         (neither-has-won position)
+         (neither-can-win position)))
+
+(define (neither-has-won position)
+  (and (not (already-won? position 'x))
+       (not (already-won? position 'o))))
+
+(define (neither-can-win position)
+  (let ((triples (find-triples position)))    
+    (not (i-can-win? triples (current-player position)))))
+
+(define (current-player position)
+  (if (> (appearances 'o position) (appearances 'x position))
+      'x
+      'o))
---
+++ 10-tictactoe/ttt.scm
@@ -0,0 +1,124 @@
+(define (ttt position me)
+  (ttt-choose (find-triples position) me))
+
+(define (ttt-choose triples me)
+  (cond ((i-can-win? triples me))
+        ((opponent-can-win? triples me))        
+        ((i-can-fork? triples me))
+        ((i-can-advance? triples me))
+        (else (best-free-square triples))))
+
+(define (find-triples position)
+  (every (lambda (comb) (substitute-triple comb position))
+           '(123 456 789 147 258 369 159 357)))
+
+(define (substitute-triple combination position)
+  (accumulate word 
+              (every (lambda (square) (substitute-letter square position))
+                     combination)))
+
+(define (substitute-letter square position)
+  (if (equal? '_ (item square position))
+      square
+      (item square position)))
+
+(define (my-pair? triple me)
+  (and (= (appearances me triple) 2)
+       (= (appearances (opponent me) triple) 0)))
+
+(define (opponent letter)
+  (if (equal? letter 'x) 'o 'x))
+
+(define (i-can-win? triples me)
+  (choose-win
+        (keep (lambda (triple) (my-pair? triple me))
+              triples)))
+
+(define (choose-win winning-triples)
+  (if (empty? winning-triples)
+      #f
+      (keep number? (first winning-triples))))
+
+(define (opponent-can-win? triples me)
+  (i-can-win? triples (opponent me)))
+
+(define (i-can-fork? triples me)
+  (first-if-any (pivots triples me)))
+
+(define (first-if-any sent)
+  (if (empty? sent)
+      #f
+      (first sent)))
+
+(define (pivots triples me)
+  (repeated-numbers (keep (lambda (triple) (my-single? triple me))
+                          triples)))
+
+(define (my-single? triple me)
+  (and (= (appearances me triple) 1)
+       (= (appearances (opponent me) triple) 0)))
+
+(define (repeated-numbers sent)
+  (every first
+         (keep (lambda (wd) (>= (count wd) 2))
+               (sort-digits (accumulate word sent)))))
+
+(define (extract-digit desired-digit wd)
+  (keep (lambda (wd-digit) (equal? wd-digit desired-digit)) wd))
+
+(define (sort-digits number-word)
+  (every (lambda (digit) (extract-digit digit number-word))
+         '(1 2 3 4 5 6 7 8 9)))
+
+(define (i-can-advance? triples me)
+  (best-move (keep (lambda (triple) (my-single? triple me)) triples)
+             triples
+             me))
+
+(define (best-move my-triples all-triples me)
+  (if (empty? my-triples)
+      #f
+      (best-square (first my-triples) all-triples me)))
+
+(define (best-square my-triple triples me)
+  (best-square-helper (pivots triples (opponent me))
+                      (keep number? my-triple)))
+
+(define (best-square-helper opponent-pivots pair)
+  (if (member? (first pair) opponent-pivots)
+      (first pair)
+      (last pair)))
+
+(define (best-free-square triples)
+  (first-choice (accumulate word triples)
+                '(5 1 3 7 9 2 4 6 8)))
+
+(define (first-choice possibilities preferences)
+  (first (keep (lambda (square) (member? square possibilities))
+               preferences)))
+
+(define (already-won? position me)
+  (member? (word me me me) (find-triples position)))
+
+(define (tie-game? position)
+    (and (<= (appearances '_ position) 2)
+         (neither-has-won position)
+         (neither-can-win position)))
+
+(define (neither-has-won position)
+  (and (not (already-won? position 'x))
+       (not (already-won? position 'o))))
+
+(define (neither-can-win position)
+  (let ((triples (find-triples position)))    
+    (not (i-can-win? triples (current-player position)))))
+
+(define (current-player position)
+  (if (> (appearances 'o position) (appearances 'x position))
+      'x
+      'o))
+
+;; test cases for the tied games
+(tie-game? 'oxoox_xo_)    ;; test if two empty spots will lead to a tie
+(tie-game? 'oxooxoxo_)    ;; test if a tie if there are two 'o shapes on a triple and one space
+(tie-game? 'oxxoxo_o_)    ;; test if a tie if there are two 'o shapes on a triple and two spaces
